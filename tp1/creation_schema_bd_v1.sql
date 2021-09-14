9668a2aeee0d6b1dcd1cfb6b62b6a9c7846014b1
Ahmed DAOUD <ahmed_daoud@ens.univ-artois.fr>
2021-09-14 16:41:58 +0200

    3e table

--- tp1/creation_schema_bd_v1.sql
+++ tp1/creation_schema_bd_v1.sql
@@ -1,13 +1,35 @@
-drop schema if exists cours CASCADE;
+DROP SCHEMA IF EXISTS compilation CASCADE;
 
-CREATE SCHEMA cours;
+CREATE SCHEMA compilation;
 
-set search_path to cours;
+SET search_path to compilation;
 
-CREATE TABLE PERSONNE (
-id serial PRIMARY KEY,
-nom varchar(150) not null,
-prenom varchar(150) not null,
-age int not null,
-ville varchar(200) default null);
+CREATE TABLE COMPOSITEUR (
+id_compositeur SERIAL PRIMARY KEY,
+nom VARCHAR(150) NOT NULL ,
+prenom VARCHAR(150) NOT NULL ,
+age INT NOT NULL ,
+ville_naissance VARCHAR(200) DEFAULT NULL );
 
+DROP TABLE IF EXISTS MUSIQUE;
+CREATE TABLE MUSIQUE(
+    id_musique SERIAL PRIMARY KEY,
+    nom VARCHAR(200) NOT NULL,
+    nom_album VARCHAR(200) DEFAULT NULL,
+    genre VARCHAR(150),
+    date_sortie DATE DEFAULT NULL
+    CHECK(id_musique>=0);
+    CHECK(LENGTH(nom)>0);
+);
+
+DROP TABLE IF EXISTS COMPOSE;
+CREATE TABLE COMPOSE(
+    id_compositeur SERIAL REFERENCES COMPOSITEUR(id_compositeur)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+    id_musique SERIAL REFERENCES MUSIQUE(id_musique)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+    PRIMARY KEY (id_compositeur,id_musique);
+    PRIMARY KEY (id_compositeur,id_musique);
+)
