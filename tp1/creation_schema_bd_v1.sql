1343b7ef63e56fa40955fca740b6c1d3b6e7b114
Ahmed DAOUD <ahmed_daoud@ens.univ-artois.fr>
2021-09-19 19:32:10 +0200

    "Ajout création_contenu"

--- tp1/creation_schema_bd_v1.sql
+++ tp1/creation_schema_bd_v1.sql
@@ -4,36 +4,37 @@ CREATE SCHEMA compilation;
 
 SET search_path to compilation;
 
-CREATE TABLE COMPOSITEUR (
-id_compositeur SERIAL PRIMARY KEY,
+CREATE TABLE CHANTEUR(
+id_chanteur SERIAL PRIMARY KEY,
 nom VARCHAR(150) NOT NULL ,
 prenom VARCHAR(150) NOT NULL ,
-age INT NOT NULL ,
+age INT NULL ,
 ville_naissance VARCHAR(200) DEFAULT NULL );
 
 DROP TABLE IF EXISTS MUSIQUE;
 CREATE TABLE MUSIQUE(
     id_musique SERIAL PRIMARY KEY,
     nom VARCHAR(200) NOT NULL,
-    nom_album VARCHAR(200) DEFAULT NULL,
+    nom_album VARCHAR(200) DEFAULT NULL UNIQUE,
     genre VARCHAR(150),
     date_sortie DATE DEFAULT NULL
-    CHECK(id_musique>=0);
-    CHECK(LENGTH(nom)>0);
+    CHECK(id_musique>=0)
+    CHECK(LENGTH(nom)>0)
 );
 
-DROP TABLE IF EXISTS COMPOSE;
-CREATE TABLE COMPOSE(
-    id_compositeur SERIAL REFERENCES COMPOSITEUR(id_compositeur)
+DROP TABLE IF EXISTS CHANTE;
+CREATE TABLE CHANTE(
+    id_chanteur SERIAL REFERENCES CHANTEUR(id_chanteur)
                     ON UPDATE CASCADE
                     ON DELETE CASCADE,
     id_musique SERIAL REFERENCES MUSIQUE(id_musique)
                     ON UPDATE CASCADE
                     ON DELETE CASCADE,
-    PRIMARY KEY (id_compositeur,id_musique);
-    PRIMARY KEY (id_compositeur,id_musique);
-)
+    PRIMARY KEY (id_chanteur,id_musique)
+);
 
 CREATE VIEW v1 AS(
-    SELECT genre, COUNT(id_musique) AS "nb_musiques", COUNT(id_compositeur) AS "nb_compositeurs" FROM Musique;
+    SELECT genre, COUNT(id_musique) AS "nb_musiques", COUNT(id_chanteur) AS "nb_compositeurs"
+    FROM Musique NATURAL JOIN CHANTE
+    GROUP BY genre
 );
