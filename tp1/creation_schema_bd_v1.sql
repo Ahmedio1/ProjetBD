482d0271d6489ac8fd028c799759f5789601789f
Ahmed DAOUD <ahmed_daoud@ens.univ-artois.fr>
2021-09-21 16:57:11 +0200

    "envoie presque final"

--- tp1/creation_schema_bd_v1.sql
+++ tp1/creation_schema_bd_v1.sql
@@ -6,37 +6,43 @@ SET search_path to compilation;
 
 DROP TABLE IF EXISTS CHANTEUR;
 CREATE TABLE CHANTEUR(
-id_chanteur SERIAL PRIMARY KEY,
+id_chanteur INT PRIMARY KEY,
 nom VARCHAR(150) NOT NULL ,
 prenom VARCHAR(150) NOT NULL ,
 age INT NULL ,
 ville_naissance VARCHAR(200) DEFAULT NULL
-CHECK(age>0));
+CHECK (id_chanteur>0)
+CHECK(age>0)
+CHECK(LENGTH(ville_naissance)>0));
 
 DROP TABLE IF EXISTS MUSIQUE;
 CREATE TABLE MUSIQUE(
-    id_musique SERIAL PRIMARY KEY,
+    id_musique INT PRIMARY KEY,
     nom VARCHAR(200) NOT NULL,
     nom_album VARCHAR(200) DEFAULT NULL,
     genre VARCHAR(150),
-    date_sortie DATE DEFAULT NULL
-    CHECK(LENGTH(nom)>0)
-    UNIQUE(nom,nom album)
+    date_sortie DATE DEFAULT NULL,
+    CHECK (id_musique>0),
+    CHECK(LENGTH(nom)>0),
+    UNIQUE(nom,nom_album)
 );
 
 DROP TABLE IF EXISTS CHANTE;
 CREATE TABLE CHANTE(
-    id_chanteur SERIAL REFERENCES CHANTEUR(id_chanteur)
+    id_chanteur INT REFERENCES CHANTEUR(id_chanteur)
                     ON UPDATE CASCADE
                     ON DELETE CASCADE,
-    id_musique SERIAL REFERENCES MUSIQUE(id_musique)
+    id_musique INT REFERENCES MUSIQUE(id_musique)
                     ON UPDATE CASCADE
                     ON DELETE CASCADE,
+    duree TIME DEFAULT NULL,
+    CHECK (duree>'00:00:00'),
     PRIMARY KEY (id_chanteur,id_musique)
 );
 
 CREATE VIEW v1 AS(
-    SELECT genre, COUNT(id_musique) AS "nb_musiques", COUNT(id_chanteur) AS "nb_compositeurs"
+    SELECT genre, COUNT(id_musique) AS "nb_musiques", COUNT(id_chanteur) AS "nb_chanteur"
     FROM Musique NATURAL JOIN CHANTE
     GROUP BY genre
+    ORDER BY genre ASC
 );
