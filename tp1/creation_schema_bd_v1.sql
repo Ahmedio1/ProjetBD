06649ed637b705a67223d9fd9569ecf69650b25e
Ahmed DAOUD <ahmed_daoud@ens.univ-artois.fr>
2021-09-21 16:28:32 +0200

    "envoie"

--- tp1/creation_schema_bd_v1.sql
+++ tp1/creation_schema_bd_v1.sql
@@ -4,22 +4,24 @@ CREATE SCHEMA compilation;
 
 SET search_path to compilation;
 
+DROP TABLE IF EXISTS CHANTEUR;
 CREATE TABLE CHANTEUR(
 id_chanteur SERIAL PRIMARY KEY,
 nom VARCHAR(150) NOT NULL ,
 prenom VARCHAR(150) NOT NULL ,
 age INT NULL ,
-ville_naissance VARCHAR(200) DEFAULT NULL );
+ville_naissance VARCHAR(200) DEFAULT NULL
+CHECK(age>0));
 
 DROP TABLE IF EXISTS MUSIQUE;
 CREATE TABLE MUSIQUE(
     id_musique SERIAL PRIMARY KEY,
     nom VARCHAR(200) NOT NULL,
-    nom_album VARCHAR(200) DEFAULT NULL UNIQUE,
+    nom_album VARCHAR(200) DEFAULT NULL,
     genre VARCHAR(150),
     date_sortie DATE DEFAULT NULL
-    CHECK(id_musique>=0)
     CHECK(LENGTH(nom)>0)
+    UNIQUE(nom,nom album)
 );
 
 DROP TABLE IF EXISTS CHANTE;
